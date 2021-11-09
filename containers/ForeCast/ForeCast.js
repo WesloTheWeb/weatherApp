dd99bb52dc2f10545358baa2543232249c0332cc
Wesley <wesleyadwebster@gmail.com>
2021-11-09 15:43:17 -0600

    env variable made, API successfully fetch

--- containers/ForeCast/ForeCast.js
+++ containers/ForeCast/ForeCast.js
@@ -5,18 +5,19 @@ import ForeCastCard from '../../components/ForecastCard/ForecastCard';
 const { forecastContainer } = classes;
 
 const Forecast = (props) => {
+
     const params = {
         apiKey: process.env.REACT_APP_WEATHER_KEY,
         baseURL: "http://api.weatherapi.com/v1/",
-        q: 53222
-    }
-    const weatherURL = `${params.baseURL}&appid=${params.apiKey}`;
+        q: 53222,
+     }
+    const currentWeather = `${params.baseURL}current.json?key=${params.apiKey}&q="San%20Francisco"&aqi=no`;
 
-    // useEffect(() => {
-    //     fetch(weatherURL)
-    //         .then( res => res.json())
-    //         .then(result => console.log(result))
-    // });
+    useEffect(() => {
+        fetch(currentWeather)
+            .then( res => res.json())
+            .then(result => console.log(result))
+    });
 
     return (
         <section className={forecastContainer}>
