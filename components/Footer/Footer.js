7187dcc30663e68b12336d0e6aea3bc464d27041
Wesley <wesleyadwebster@gmail.com>
2021-11-08 22:03:43 -0600

    format styling

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,16 +5,10 @@ import styles from './Footer.module.css';
 const Footer = (props) => {
     return (
         <footer className={styles.footer}>
-            <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Powered by{' '}
-                <span className={styles.logo}>
-                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                </span>
-            </a>
+            Powered by{' '}
+            <span className={styles.logo}>
+                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            </span>
         </footer>
     );
 };
